dfc1d0eafe7ba43a6edfa2d6a42a2cf78ebd2ebc
Owais Raza <143530826+Owais276raza@users.noreply.github.com>
2024-05-14 15:41:47 +0500

    Add files via upload

---
+++ Web Application/app.js
@@ -0,0 +1,49 @@
+var onScreenElement = document.getElementById("OnScreen");
+var even_odd = document.getElementById("even_odd");
+
+var onScreenValue = 0;
+
+function plus(){
+   onScreenValue++;
+  onScreenElement.innerText = onScreenValue; 
+console.log(onScreenValue);
+
+if (onScreenValue % 2 === 0) {
+    even_odd.innerText = "Even";
+} else {
+    even_odd.innerText = "Odd";
+}
+if(onScreenValue < 0){
+    onScreenValue = 0;
+    onScreenElement.innerText = onScreenValue;
+
+    
+}
+
+}
+
+function munies(){
+    onScreenValue--;
+    onScreenElement.innerText = onScreenValue;
+    console.log(onScreenValue);
+
+    if (onScreenValue % 2 === 0) {
+        even_odd.innerText = "Even";
+    } else {
+        even_odd.innerText = "Odd";
+    }
+    if(onScreenValue < 0){
+        onScreenValue = 0;
+        alert("Number Cannot Be Less Than 0")
+        onScreenElement.innerText = onScreenValue;
+        console.log(onScreenValue);
+        
+    }
+}
+
+function reset(){
+    onScreenValue = 0;
+    onScreenElement.innerText = onScreenValue;
+    console.log(onScreenValue);
+    
+}
